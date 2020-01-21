5accee22f6e16451f4d675a52d705a95e401579f
KJnong <57115252+KJnong@users.noreply.github.com>
2020-01-21 23:25:30 +0200

    edited UI with bootstrap

---
+++ DL-Bank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DL_Bank.Startup))]
+namespace DL_Bank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
