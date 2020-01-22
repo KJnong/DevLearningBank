45386ab9dbacb591f103c08a88edcb456b5d2494
KJnong <57115252+KJnong@users.noreply.github.com>
2020-01-22 02:41:18 +0200

    added validation to CheckAccount model

--- DL-Bank/Models/CheckingAccount.cs
+++ DL-Bank/Models/CheckingAccount.cs
@@ -10,12 +10,17 @@ namespace DL_Bank.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        //[StringLength(10, MinimumLength =10)]
+        [RegularExpression(@"\d{10}", ErrorMessage ="Account # must contain 10 digits")]
         [Display(Name ="Account #")]
         public string AccountNumber { get; set; }
 
+        [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
