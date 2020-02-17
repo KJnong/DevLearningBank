e71c635c89516788558e7f36c796cebb9bece32a
KJnong <57115252+KJnong@users.noreply.github.com>
2020-02-17 15:25:40 +0200

    Deployement

--- DL-Bank/Migrations/Configuration.cs
+++ DL-Bank/Migrations/Configuration.cs
@@ -14,6 +14,7 @@ namespace DL_Bank.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "DL_Bank.Models.ApplicationDbContext";
         }
 
